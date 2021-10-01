1094a7024d21e2e42bebe0cb7aa28f374a354961
Rohit <jaiswalrohit8319@gmail.com>
2021-10-01 21:07:54 +0530

    added sign up and sign in pages and some features

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var j;
+    for (j = 0; j < dropdowns.length; j++) {
+      var openDropdown = dropdowns[j];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
